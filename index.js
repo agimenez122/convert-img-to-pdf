a55799b6e255702eb0c6abee88915b7a457be20d
agimenez122 <andresgimpe@gmail.com>
2024-05-23 23:46:50 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const PDFDocument = require('pdfkit');
+const path = require('path');
+const fs = require('fs');
+
+
+// ArgumentsW
+if (process.argv.length < 3) {
+  console.error('Uso: node script.js <nombre_del_archivo_de_salida.pdf>');
+  process.exit(1);
+}
+
+// Params
+const pdfFilePath = `out/${process.argv[2]}.pdf`;
+const pngDir = './input/'; 
+
+const doc = new PDFDocument();
+const writeStream = fs.createWriteStream(pdfFilePath);
+doc.pipe(writeStream);
+
+// Generate pdf doc
+fs.readdir(pngDir, (err, files) => {
+  if (err) {
+    return console.error(`No se pudo leer el directorio: ${err.message}`);
+  }
+
+  const imageFiles = files.filter(file => ['.png', '.jpg'].includes(path.extname(file).toLowerCase()));
+
+  imageFiles.forEach((imageFile, index) => {
+    if (index > 0) {
+      doc.addPage();
+    }
+
+    const imagePath = path.join(pngDir, imageFile);
+
+    doc.image(imagePath, {
+      fit: [500, 700], 
+      align: 'center',
+      valign: 'center'
+    });
+  });
+
+  doc.end();
+});
+
+writeStream.on('finish', () => {
+  console.log(`Archivo PDF creado: ${pdfFilePath}`);
+});
